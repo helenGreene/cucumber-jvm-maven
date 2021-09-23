5d5e83d1a019f5a385fb57bb17de76427e0db692
Jane Jin(Bingyu) <jbyjane@qq.com>
2021-09-23 15:06:13 +0800

    update feature path format

--- src/test/java/com/czeczotka/bdd/runner/RunCalculatorTest.java
+++ src/test/java/com/czeczotka/bdd/runner/RunCalculatorTest.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
                 "pretty"
         },
         glue = "com.czeczotka.bdd.steps",
-        features="classpath:cucumber/calculator.feature"
+        features="src/test/resources/cucumber/calculator.feature"
         //src/test/resources/cucumber/calculator.feature
         //classpath:cucumber/calculator.feature
 )
