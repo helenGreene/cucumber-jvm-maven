40163614ce35a29561688e4a6cc1c6cb7db1002f
Jane Jin(Bingyu) <jbyjane@qq.com>
2021-09-23 14:34:55 +0800

    update pom.xml

--- src/test/java/com/czeczotka/bdd/runner/RunCalculatorTest.java
+++ src/test/java/com/czeczotka/bdd/runner/RunCalculatorTest.java
@@ -1,14 +1,21 @@
 package com.czeczotka.bdd.runner;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        format = { "pretty", "html:target/cucumber" },
+        plugin = {
+                "junit:target/junit-cucumber-reports/calculatortwo.xml",
+                "html:target/cucumber",
+                "pretty"
+        },
         glue = "com.czeczotka.bdd.steps",
-        features = "classpath:cucumber/calculator.feature"
+        features="classpath:cucumber/calculator.feature"
+        //src/test/resources/cucumber/calculator.feature
+        //classpath:cucumber/calculator.feature
 )
 public class RunCalculatorTest {
 }
--- src/test/java/com/czeczotka/bdd/steps/CalculatorSteps.java
+++ src/test/java/com/czeczotka/bdd/steps/CalculatorSteps.java
@@ -1,10 +1,10 @@
 package com.czeczotka.bdd.steps;
 
 import com.czeczotka.bdd.calculator.Calculator;
-import cucumber.api.java.Before;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
